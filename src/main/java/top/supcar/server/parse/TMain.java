ca55ab6108b27f3f075545fdaa0626ed3615842e
rayaraya <raya.orlova.98@mail.ru>
2017-04-17 23:36:55 +0300

    add wsserver

--- src/main/java/top/supcar/server/parse/TMain.java
+++ src/main/java/top/supcar/server/parse/TMain.java
@@ -10,8 +10,8 @@ import java.util.Map;
 public class TMain {
     public static void main(String[] args) throws Exception {
 
-        /*String url = "http://www.overpass-api.de/api/xapi?way[bbox=30.258916543827283,59.917968282222404,30.34371726404213,59.94531882096226]";
-        Map<String,Way> roads;
+        String url = "http://www.overpass-api.de/api/xapi?way[bbox=30.258916543827283,59.917968282222404,30.34371726404213,59.94531882096226]";
+        /*Map<String,Way> roads;
         OSMData data = new OSMData(url);
         data.loadData();
         data.makeMap();
