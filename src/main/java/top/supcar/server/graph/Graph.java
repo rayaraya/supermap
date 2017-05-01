f55845b7512622eaac4e4277850465421e67bec6
nataboll <nataboll@mail.ru>
2017-05-01 22:28:57 +0300

    +Dijkstra

--- src/main/java/top/supcar/server/graph/Graph.java
+++ src/main/java/top/supcar/server/graph/Graph.java
@@ -1,75 +1,135 @@
 package top.supcar.server.graph;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
 import info.pavie.basicosmparser.model.*;
 import info.pavie.basicosmparser.model.Node;
 import top.supcar.server.parse.OSMData;
 
-
-public class Graph {
-    private static final double R = 6371000; // Earth's radius
+public class Graph extends PriorityQueue{
+    private static final double R = 6371000;
     private static final double TRANS = Math.PI/180;
-    public Map <Node, List<Node>> adjList;
+    private Map<Node, List<Node>> adjList;
     private Map<String, Way> interMap;
     private Map<String, Double> weightList;
+    private List<Node> vertexList;
+    private Map<Node, List<Way>> nodeWays;
 
     public void setInterMap(Map<String,Way> map) {
         interMap = map;
     }
 
-    private Map<String, Way> getInterMap(){
+    public Map<String, Way> getInterMap(){
         return this.interMap;
     }
 
     public void setMap(){
+
+        adjList = new HashMap<>();
         Iterator<Map.Entry<String, Way>> interMapIter = interMap.entrySet().iterator();
         Iterator<Node> roadIter;
         Map.Entry<String, Way> currEntry;
-        Element currElement;
+        Way currWay;
         List<Node> road;
-        List<Node> vertexList = new ArrayList<>();  // список вершин
 
-        while (interMapIter.hasNext()){
+        vertexList = new ArrayList<>();
+        weightList = new HashMap<>();
+
+        while (interMapIter.hasNext()) {
             currEntry = interMapIter.next();
-            currElement = currEntry.getValue();
-            road = ((Way) currElement).getNodes();
+            currWay = currEntry.getValue();
+            String tmp = currWay.getTags().get("oneway");
+            boolean oneway = (tmp == null) || (tmp.equals("no"));
+
+            road = currWay.getNodes();
+
             roadIter = road.listIterator();
-            Node currNode = roadIter.next();
-         //   System.out.println(currNode);
-            vertexList.add(currNode);
+            Node currNode;
             Node nextNode;
 
-            while (roadIter.hasNext()){
-                boolean isAdded = false;
-                if (!(vertexList.contains(currNode))) {
-                    vertexList.add(currNode);
-                    isAdded = true;
-                }
-                nextNode = roadIter.next();
-                List<Node> toAdd = new ArrayList<>();
-                double currDistance = getDistance(currNode, nextNode);
-                if (isAdded) {
-                    toAdd.add(nextNode);
-                    adjList.put(currNode, toAdd);
-                }
-                else {
-                    try {   // непонятный NullPointerException
+            if (roadIter.hasNext()) {
+                currNode = roadIter.next();
+
+                while (roadIter.hasNext()) {
+                    /*boolean isAdded = false;
+                    if (!(vertexList.contains(currNode))) {
+                        vertexList.add(currNode);
+                        isAdded = true;
+                    }*/
+                    nextNode = roadIter.next();
+
+                    //double currDistance = getDistance(currNode, nextNode);
+                    /*if (isAdded) {
+                        toAdd = new ArrayList<>();
+                        toAdd.add(nextNode);
+                        adjList.put(currNode, toAdd);
+                    } else {
                         toAdd = adjList.get(currNode);
-                    } catch (Exception ex) {
-                        System.out.println(ex);
+                        // adjList.remove(currNode);
+                        toAdd.add(nextNode);
+                        //  adjList.put(currNode, toAdd);
+                    }*/
+                    addInAdjList(currNode, nextNode);
+
+
+
+             /*   String key = currNode.getId() + nextNode.getId();
+                weightList.put(key, currDistance);*/
+
+                    if (oneway) {
+                        addInAdjList(nextNode, currNode);
+                       /* isAdded = false;
+                        if (!(vertexList.contains(nextNode))) {
+                            vertexList.add(nextNode);
+                            isAdded = true;
+                        }
+                        if (isAdded) {
+                            toAdd = new ArrayList<>();
+                            toAdd.add(currNode);
+                            adjList.put(nextNode, toAdd);
+                        } else {
+                            toAdd = adjList.get(nextNode);
+                            adjList.remove(nextNode);
+                            toAdd.add(currNode);
+                            adjList.put(nextNode, toAdd);
+                        }
+         /*               key = nextNode.getId() + currNode.getId();
+                        weightList.put(key, currDistance);      */
                     }
-                  //  adjList.remove(currNode);     // не работают никакие функции с adjList
-                    toAdd.add(nextNode);
-                  //  adjList.put(currNode, toAdd);
+
+                    currNode = nextNode;
                 }
-                String key = currNode.getId() + nextNode.getId();
-                weightList.put(key, currDistance);
+
+            }
+        }
+/*        System.out.println("adjList: " + adjList.size());
+        System.out.println("vertexList: " + vertexList.size());
+        Iterator<Map.Entry<Node,List<Node>>> itt = adjList.entrySet().iterator();
+        Map.Entry<Node,List<Node>> entr;
+        List<Node> dbgList;
+        Node src;
+
+        while(itt.hasNext()) {
+            entr = itt.next();
+            src = entr.getKey();
+            dbgList = entr.getValue();
+            for(Node nd: dbgList) {
+                if(!weightList.containsKey(src.getId()+nd.getId())) {
+                    System.out.println(src.getId() + " " + nd.getId());
                 }
             }
-   // System.out.println(vertexList);
+        }   */
+
+
+    }
+
+    private void addInAdjList(Node a, Node b) {
+        List<Node> adjNodes = adjList.get(a);
+        if(adjNodes == null) {
+            adjNodes = new ArrayList<>();
+            adjList.put(a, adjNodes);
+        }
+        adjNodes.add(b);
     }
 
     private double getDistance(Node currNode, Node nextNode) {
@@ -87,22 +147,133 @@ public class Graph {
         return  R*p;
     }
 
+    public Map<String, Double> getWeightList(){
+        return weightList;
+    }
+
+    public Map<Node, List<Node>> getAdjList(){
+        return this.adjList;
+    }
+
+ /*   public List<Node> getWay(Node first, Node last){
+        Iterator<Map.Entry<String, Way>> interMapIter = interMap.entrySet().iterator();
+        Map.Entry<String, Way> currEntry;
+        Element currElement;
+        List<Node> road;
+        currEntry = interMapIter.next();
+        currElement = currEntry.getValue();
+        road = ((Way) currElement).getNodes();
+
+        while ((interMapIter.hasNext())&&(road.size()<10)) {
+            currEntry = interMapIter.next();
+            currElement = currEntry.getValue();
+            road = ((Way) currElement).getNodes();
+        }
+
+       // System.out.println(road);
+        return road;
+    }   */
+
+
+
+    public void setVertexList() {
+        Iterator<Map.Entry<Node, List<Node>>> listIter = adjList.entrySet().iterator();
+        while (listIter.hasNext()) {
+            Node cNode = listIter.next().getKey();
+            Node nNode;
+            List<Node> road = adjList.get(cNode);
+            Iterator roadIter = road.iterator();
+            if (!vertexList.contains(cNode))
+                vertexList.add(cNode);
+
+            while (roadIter.hasNext()){
+                nNode = (Node) roadIter.next();
+                if (!vertexList.contains(nNode))
+                    vertexList.add(nNode);
+            }
+        }
+    }
+
+    public List<Node> getVertexList(){
+        return vertexList;
+    }
+
+    public void setWeightList(){
+        Iterator<Map.Entry<Node, List<Node>>> listIter = adjList.entrySet().iterator();
+        while (listIter.hasNext()) {
+            Node cNode = listIter.next().getKey();
+            Node nNode;
+            List<Node> road = adjList.get(cNode);
+            Iterator roadIter = road.iterator();
+
+            while (roadIter.hasNext()){
+                nNode = (Node) roadIter.next();
+                weightList.put(cNode.getId()+nNode.getId(), getDistance(cNode, nNode));
+            }
+        }
+    }
+
+    public void setNodeWays(){
+        nodeWays = new HashMap<>();
+        Iterator<Map.Entry<String, Way>> interMapIter = interMap.entrySet().iterator();
+        Iterator<Node> roadIter;
+        Map.Entry<String, Way> currEntry;
+        Way currWay;
+        List<Node> road;
+
+        while (interMapIter.hasNext()) {
+            currEntry = interMapIter.next();
+            currWay = currEntry.getValue();
+            road = currWay.getNodes();
+
+            roadIter = road.listIterator();
+            Node currNode;
+
+            while (roadIter.hasNext()){
+                currNode = roadIter.next();
+                List<Way> wayList = nodeWays.get(currNode);
+                if (wayList==null){
+                    wayList = new ArrayList<>();
+                    nodeWays.put(currNode, wayList);
+                }
+                wayList.add(currWay);
+            }
+        }
+    }
+
+    public Map<Node, List<Way>> getNodeWays(){
+        return nodeWays;
+    }
+
 
     public static void main(String[] args){
         Graph graph = new Graph();
-        OSMData data = new OSMData();
-        data.loadData();
+        String url = "http://www.overpass-api.de/api/xapi?way[bbox=30.258916543827283,59.917968282222404,30.34371726404213,59.94531882096226]";
+        OSMData data = new OSMData(url);
+        //data.loadData();
         data.makeMap();
         Map<String, Way> testMap;
         testMap = data.getMap();
         graph.setInterMap(testMap);
         Map<String, Way> m;
         m = graph.getInterMap();
-     //   System.out.println(m);
         graph.setMap();
-    }
-
-    public Map<String, Double> getWeightList(){
-        return weightList;
+        graph.setVertexList();
+     //   System.out.println(graph.getAdjList().size());
+     //   System.out.println(graph.getVertexList().size());
+        graph.setWeightList();
+        graph.setNodeWays();
+        Node first = graph.vertexList.get(100);
+        System.out.println(graph.getNodeWays());
+     //   System.out.println(graph.getWeightList().size());
+     /*   Map<Node, List<Node>> adj = graph.getAdjList();
+    //    System.out.println(adj);
+        Map<String, Double> w = graph.getWeightList();
+        Node first = graph.vertexList.get(1);
+        Node last = graph.vertexList.get(2);
+ //       List<Node> road = graph.getWay(first,last);
+ //       System.out.println(road);
+ //       System.out.println(road.size());
+ */
     }
 }
